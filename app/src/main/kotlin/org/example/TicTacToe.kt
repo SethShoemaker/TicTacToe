b42fefdf3960a01a40c1870169d7f68f22f2e735
seth shoemaker <contact@sethshoemaker.com>
2024-03-17 17:09:54 -0400

    update getTicTacToeWinner comment

--- app/src/main/kotlin/org/example/TicTacToe.kt
+++ app/src/main/kotlin/org/example/TicTacToe.kt
@@ -2,10 +2,26 @@ package org.example;
 
 object TicTacToe{
     /**
-     * In a game of tic-tac-toe, there are a total of 9 boxes.
+     * Given the state of a tic tac toe game, get the winner of the game if one exists.
+     * 
+     * Game state:
+     *  The state of the tic tac toe game is represented by a list of 9 integers.
+     *  0 signifies that no player has filled in the box, 1 signifies that player 1 has filled in the box, and 2 signifies that player 2 has filled in the box.
+     * 
+     * Approach used:
+     *  The apprach used can be broken down into the following steps:
+     *  1) Iterate through each column in the board, if a winner can be found from a column, add it to the list of winners
+     *  2) Iterate through each row in the board, if a winner can be found from a row, add it to the list of winners
+     *  3) Check the diagonal going from the top left corner to the bottom right corner for a winner, if a winner is found, add it to the list of winners
+     *  4) Check the diagonal going from the bottom left corner to the top right corner for a winner, if a winner is found, add it to the list of winners
+     *  5) After checking all of the possible ways a player can win the game, inspect the list of winners. 
+     *      If there is more than 1 winner, throw an excpetion. There should only ever be one winner at a game of tic tac toe.
+     *      If there is no winner return 0. This means no player has won the game in its current state.
+     *      If there is 1 winner, return that player's number (either 1 or 2).
      *
-     *
-     * 0 signifies that no player has filled in the box, 1 signifies that player 1 has filled in the box, and 2 signifies that player 2 has filled in the box.
+     * @param boxValues the list of 9 integers which represent the game state
+     * @returns the winning player if one exists. If a winning player does not exist, 0 will be returned.
+     * @throws Exception if the input size is not equal to 9, if the input contains integers which are not 0, 1, or 2, or if there are multiple winners of the game.
      */
     fun getTicTacToeWinner(boxValues: List<Int>): Int{
     
